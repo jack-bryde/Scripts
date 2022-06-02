827d771769c1ca63b90db7b46a3d3a9ee0dca79c
jack-bryde <106286328+jack-bryde@users.noreply.github.com>
2022-06-02 13:39:43 +1000

    Create Fix_bad_csv.ps1

---
+++ PowerShell/Fix_bad_csv.ps1
@@ -0,0 +1,8 @@
+# A large .csv file that contained " as a field value, breaking upon import by Excel
+# (works one file at a time, put in loop for folder)
+$ADT = Get-ChildItem "T:\file_path\file.csv"
+$content = Get-Content $ADT.FullName -Raw
+$newName = $ADT.Name.substring($ADT.Name.Length - 15, 15).Replace("_", " ")
+# Replace the miscreant chars with a word and save modified file with new name to show it has been adjusted
+$content.Replace('"',"QuoteMark") | Set-Content -Path $newName
+Set-Content $newName
