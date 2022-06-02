3c0a2cab26a5758d69a390be504e34b8610065d3
jack-bryde <106286328+jack-bryde@users.noreply.github.com>
2022-06-02 13:44:12 +1000

    Update Fix_bad_csv.ps1

--- PowerShell/Fix_bad_csv.ps1
+++ PowerShell/Fix_bad_csv.ps1
@@ -2,6 +2,7 @@
 # (works one file at a time, put in loop for folder)
 $ADT = Get-ChildItem "T:\file_path\file.csv"
 $content = Get-Content $ADT.FullName -Raw
+# New name removed the datestamp in each file
 $newName = $ADT.Name.substring($ADT.Name.Length - 15, 15).Replace("_", " ")
 # Replace the miscreant chars with a word and save modified file with new name to show it has been adjusted
 $content.Replace('"',"QuoteMark") | Set-Content -Path $newName
